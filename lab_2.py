d631fce284cc2a9a65fa47ac1775981967350efd
Merlliya <s.lashenko.56@gmail.com>
2022-06-13 19:35:58 +0900

    объединяем изображения и выводим на рабочий стол

--- lab_2.py
+++ lab_2.py
@@ -15,3 +15,7 @@ req("https://anzu.sinc.ad.jp/himawari/img/FULL_24h/BlueMarble/1d/770/BlueMarble_
 req("https://jh170034-2.kudpc.kyoto-u.ac.jp/img/FULL_24h/B13/1d/550/2022/06/12/092000_0_0.png", "2.png") #вызов функции для получения второго изображения
 image1 = Image.open('1.png').convert('RGBA') #конвертация изображения в прозрачный RGB
 image2 = Image.open('2.png').convert('RGBA').resize((image1.size[0], image1.size[1])) #конвертация изображения в прозрачный RGB и изменение размера
+image1.paste(image2, (0, 0), image2) #объединение двух изображений 1 и 2
+image1 = image1.resize((int(image1.size[0] * (GetSystemMetrics(1) / image1.size[1])), GetSystemMetrics(1)))#изменение размера
+image1.save('wallpaper.png') #сохранние готовго изображения
+ctypes.windll.user32.SystemParametersInfoW(20, 0, os.path.abspath('wallpaper.png'), 0) #установка wallpaper.png на фон рабочего стола
