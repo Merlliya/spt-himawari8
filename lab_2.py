9a70c888794b93aff5a5d5b4afccb07ca2b5ee4f
Merlliya <s.lashenko.56@gmail.com>
2022-06-13 18:58:30 +0900

    отправка запроса и ответа на изображения

--- lab_2.py
+++ lab_2.py
@@ -6,3 +6,7 @@ from PIL import Image
 from win32api import GetSystemMetrics
 
 
+def req(link, name): #фунция отправки запросов
+    answer = requests.get(link).content #ответ от запроса
+    with open(name, 'wb') as handler:
+        handler.write(answer) #сохранение изображения получаемого в ответе запроса
