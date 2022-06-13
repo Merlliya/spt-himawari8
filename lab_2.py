990c37643f8c609557da9ac5549a5cd21686f3f3
Merlliya <s.lashenko.56@gmail.com>
2022-06-13 19:23:16 +0900

    получаем изображения и конвертируем

--- lab_2.py
+++ lab_2.py
@@ -10,3 +10,8 @@ def req(link, name): #фунция отправки запросов
     answer = requests.get(link).content #ответ от запроса
     with open(name, 'wb') as handler:
         handler.write(answer) #сохранение изображения получаемого в ответе запроса
+
+req("https://anzu.sinc.ad.jp/himawari/img/FULL_24h/BlueMarble/1d/770/BlueMarble_0_0.png", "1.png") #вызов функции для получения первого изображения
+req("https://jh170034-2.kudpc.kyoto-u.ac.jp/img/FULL_24h/B13/1d/550/2022/06/12/092000_0_0.png", "2.png") #вызов функции для получения второго изображения
+image1 = Image.open('1.png').convert('RGBA') #конвертация изображения в прозрачный RGB
+image2 = Image.open('2.png').convert('RGBA').resize((image1.size[0], image1.size[1])) #конвертация изображения в прозрачный RGB и изменение размера
